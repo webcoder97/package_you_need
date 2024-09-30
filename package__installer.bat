1d3b8f259d333e64f5acc7b003cf6991597f9345
Md. Saiful <99985697+webcoder97@users.noreply.github.com>
2024-09-30 17:17:39 +0600

    Add files via upload
    update with user prompt

--- package__installer.bat
+++ package__installer.bat
@@ -1,34 +1,68 @@
 @echo off
-echo Installing Microsoft Visual Studio Code
+
+setlocal
+
+:install_vscode
+echo Installing Microsoft Visual Studio Code? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_figma
 winget install Microsoft.VisualStudioCode --silent
 
-echo Installing Figma
+:install_figma
+echo Installing Figma? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_nodejs
 winget install Figma.figma --silent
 
-echo Installing NodeJS
+:install_nodejs
+echo Installing NodeJS? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_github_cli
 winget install -e --id OpenJS.NodeJS --silent
 
-echo Installing GitHub CLI
+:install_github_cli
+echo Installing GitHub CLI? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_firefox
 winget install GitHub.cli --silent
 
-echo Installing Firefox Developer Edition
+:install_firefox
+echo Installing Firefox Developer Edition? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_git
 winget install Mozilla.Firefox.DeveloperEdition --silent
 
-echo Installing Git
+:install_git
+echo Installing Git? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_winrar
 winget install Git.Git --silent
 
-echo Installing WinRAR
+:install_winrar
+echo Installing WinRAR? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_github_desktop
 winget install -e --id RARLab.WinRAR --silent
 
-echo Installing GitHub Desktop
+:install_github_desktop
+echo Installing GitHub Desktop? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_sublime
 winget install -e --id GitHub.GitHubDesktop --silent
 
-echo Installing Sublime Text 4
+:install_sublime
+echo Installing Sublime Text 4? (Y/N)
+choice /c YN
+if errorlevel 2 goto install_notepad
 winget install -e --id SublimeHQ.SublimeText.4 --silent
 
-echo Installing Notepad++
+:install_notepad
+echo Installing Notepad++? (Y/N)
+choice /c YN
+if errorlevel 2 goto end
 winget install -e --id Notepad++.Notepad++ --silent
 
+:end
 echo All installations are complete.
 echo This Package Made By Md. Saiful - https://github.com/webcoder97
 pause
